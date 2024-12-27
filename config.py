208cc674b1f6f1ed0c848e977d658ec5c009f133
1irotech <irotech015@gmail.com>
2024-12-27 20:02:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,15 +8,15 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "7921939461:AAGOcCoi9yX8OjPmJs-jzaEEbAuwRZ_ufSA")
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7520092354"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/yourtoofan/CHATBOT")
+OWNER_ID = int(getenv("OWNER_ID", "7086360370"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/1irotech/CHATBOT")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-SUPPORT_GRP = "THE_INCRICIBLE"
-UPDATE_CHNL = "THE_INCRICIBLE"
-OWNER_USERNAME = "LEGEND_MICKEY"
+SUPPORT_GRP = "ironmanhindigming"
+UPDATE_CHNL = "irotechbotz0"
+OWNER_USERNAME = "Ironmanhindigaming"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
     
